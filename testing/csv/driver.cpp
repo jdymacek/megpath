9eb1b6980764264c06b1ed70f6bb406ba2ef8e45
DaedricMD <dragonborn2649@gmail.com>
2017-05-23 15:35:03 -0400

    added a mix type case

--- testing/csv/driver.cpp
+++ testing/csv/driver.cpp
@@ -39,6 +39,24 @@ int main(){
 		cout << "\n";
 	}
 
+	cout << "\nMixed types: \n"; //string,int,double
+	vec = file.readCSV("mixed.txt");
+	for(int i = 0; i < vec.size(); ++i){
+		int switcher = 0;
+		for(int j = 0; j < vec[i].size(); ++j){
+			if(switcher == 0){
+				cout << vec[i][j].asString() << " ";
+				++switcher;
+			}else if(switcher == 1){	
+				cout << vec[i][j].asInt() << " ";
+				++switcher;
+			}else{
+				cout << vec[i][j].asDouble() << " ";
+			}
+		}
+		cout << "\n";
+	}
+
 	cout << "\n";
 
 	return 0;
