93a28d6687e662dfe3bc27a2a6a3dc7a3c9cdd3a
Julian Dymacek <jdymacek@gmail.com>
2017-05-23 09:43:56 -0400

    asString, asDouble, asInt

--- shared/Value.cpp
+++ shared/Value.cpp
@@ -8,27 +8,33 @@ Value::Value(string d){
 	data = d;
 }
 
-int Value::asInt(){
-	return -1;
+int Value::asInt() const{
+	string str = data;
+	return atoi(str.c_str());
 }
 
-double Value::asDouble(){
-	return -1;
+double Value::asDouble() const{
+	string str = data;
+	return atof(str.c_str());
 }
 
-string Value::asString(){
-	return data;
+string Value::asString() const{
+	if(data.size() <= 2){
+		return "";
+	}
+	if(data[0] != '\"' || data[data.size()-1] != '\"'){
+		return "";
+	}
+	return  data.substr(1,data.size()-2);
 }
 
-vector<Value> Value::asVector(){
+vector<Value> Value::asVector() const{
 	vector<Value> vec;
 	//check for the []
 	if(data.size() > 0){
 		//parse string?
 
 	}
-
-
 	return vec;
 }
 
