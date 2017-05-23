4c04454fcae8b20cce2bab58ce84a3bca4563433
DaedricMD <dragonborn2649@gmail.com>
2017-05-23 16:48:10 -0400

    ProbFunc skeleton

---
+++ shared/ProbFunc.h
@@ -0,0 +1,26 @@
+//ProbFunc.h
+//Matthew Dyer
+//Created on 5/23/2017
+//Last Modified: 5/23/2017
+
+#ifndef PROBFUNC__H
+#define PROBFUNC__H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Entry{
+	int x;
+	int y;
+	double v;
+}
+
+class ProbFunc{
+	virtual vector<Entry> random() = 0;
+	virtual void addObservation(double d) = 0;
+	virtual string toString() = 0;
+};
+
+#endif
