db5d58641cefb3576e1ae2d95954ca82354b27dc
DaedricMD <dragonborn2649@gmail.com>
2017-05-23 15:24:19 -0400

    added more csv tests

--- testing/csv/driver.cpp
+++ testing/csv/driver.cpp
@@ -12,14 +12,34 @@ using namespace std;
 int main(){
 	CSVFile file;
 
-	vector<vector<Value> > vec = file.readCSV("test.txt");
-
+	cout << "Integers only: \n";
+	vector<vector<Value> > vec = file.readCSV("ints.txt");
 	for(int i = 0; i < vec.size(); ++i){
 		for(int j = 0; j < vec[i].size(); ++j){
-			cout << vec[i][j].toString() << " ";
+			cout << vec[i][j].asInt() << " ";
 		}
 		cout << "\n";
 	}
+
+	cout << "\nDoubles only: \n";
+	vec = file.readCSV("doubles.txt");
+	for(int i = 0; i < vec.size(); ++i){
+		for(int j = 0; j < vec[i].size(); ++j){
+			cout << vec[i][j].asDouble() << " ";
+		}
+		cout << "\n";
+	}
+
+	cout << "\nStrings only: \n";
+	vec = file.readCSV("strings.txt");
+	for(int i = 0; i < vec.size(); ++i){
+		for(int j = 0; j < vec[i].size(); ++j){
+			cout << vec[i][j].asString() << " ";
+		}
+		cout << "\n";
+	}
+
+	cout << "\n";
 
 	return 0;
 }
