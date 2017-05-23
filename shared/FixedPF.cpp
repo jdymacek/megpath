69a29376249dc156d11e1515d257228e2a41eb69
Julian Dymacek <jdymacek@gmail.com>
2017-05-23 16:20:19 -0400

    created FixedPF.cpp

---
+++ shared/FixedPF.cpp
@@ -0,0 +1,20 @@
+#include "FixedPF.h"
+
+
+
+FixedPF::FixedPF(){
+
+}
+
+
+vector<Entry> FixedPF::random(){
+	return vec;
+}
+
+string FixedPF::toString(){
+	return "";
+}
+
+void FixedPF::addObservation(double d){
+
+}
