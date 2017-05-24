d1431b92994853f40e205b9fbaf645c03b2847fd
DaedricMD <dragonborn2649@gmail.com>
2017-05-24 10:38:54 -0400

    added functions defintions for other tests

--- testing/probfunc/driver.cpp
+++ testing/probfunc/driver.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 void fixedTest(){
-	ProbFunc* pf = new FixedPF(1,1,0.5);
+	ProbFunc* pf = new FixedPF(0,1,0.5);
 	for(int i = 0; i < 1000; ++i){
 		vector<Entry> vec = pf->random();
 		if(vec[0].val <= 0.495 || vec[0].val >= 0.505){
@@ -20,11 +20,31 @@ void fixedTest(){
 
 }
 
+void histoTest(){
+
+}
+
+void shiftTest(){
+
+}
+
+void uniformTest(){
+
+}
+
+void normalTest(){
+
+}
+
 int main(int argc, char* argv[]){
 
 	if(argc < 2){
 		cout << "Not enough arguments!\nOptions:\n"
-		     << "-fixed\n";
+		     << "-fixed\n"
+		     << "-histo\n"
+		     << "-uniform\n"
+		     << "-shift\n"
+		     << "-normal\n";
 		exit(1);
 	}
 
@@ -33,7 +53,14 @@ int main(int argc, char* argv[]){
 
 	if(type == "-fixed"){
 		fixedTest();
+	}else if(type == "-histo"){
+		histoTest();
+	}else if(type == "-uniform"){
+		uniformTest();
+	}else if(type == "-shift"){
+		shiftTest();
+	}else{
+		normalTest();
 	}
-
 	return 0;
 }
