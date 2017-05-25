22f78799ee9c6d6eb608f88abc4d98c8266e6f7b
Julian Dymacek <jdymacek@gmail.com>
2017-05-25 10:11:44 -0400

    updating HistoPF

--- shared/HistoPF.h
+++ shared/HistoPF.h
@@ -1,27 +1,27 @@
 //HistoPF.h -- Histogram Based Probability Function
 //Julian Dymacek
 //Created 5/24/2017
-//Modified 5/24/2017
+//Modified 5/25/2017
 #ifndef HISTOPF__H
 #define HISTOPF__H
 
 #include <vector>
 #include <random>
+#include <sstream>
 #include "ProbFunc.h"
 
 using namespace std;
 
 class HistoPF: public ProbFunc{
 	public:
-		HistoPF(int x,int y);
-		vector<Entry> random();
+		HistoPF();
+		double random();
 		void addObservation(double d);
 		string toString();
 	private:
 		piecewise_linear_distribution<double> dist;
 		vector<double> intervals;
 		vector<double> weights;
-		vector<Entry> value;
 };
 
 #endif
