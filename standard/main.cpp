0ed5fdf755585d6ff1765ab57261c612085b44e1
DaedricMD <dragonborn2649@gmail.com>
2017-05-25 10:52:18 -0400

    added includes

--- standard/main.cpp
+++ standard/main.cpp
@@ -1,4 +1,18 @@
 //Main file
 //Matthew Dyer
 //Created on 5/25/2017
-//Last modified 5/25/2017
+//Last modified: 5/25/2017
+
+#include<iostream>
+#include"../shared/Value.h"
+#include"../shared/ArgFile.h"
+#include"../shared/CSVFile.h"
+
+using namespace std;
+
+int main(){
+	ArgFile args;
+	args.load("arguments.txt");
+	
+	return 0;
+}
