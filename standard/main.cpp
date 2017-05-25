8858240cdaf4713369cb8dfd5d5bd84d354f813c
DaedricMD <dragonborn2649@gmail.com>
2017-05-25 11:11:20 -0400

    fixed compile issues

--- standard/main.cpp
+++ standard/main.cpp
@@ -4,9 +4,9 @@
 //Last modified: 5/25/2017
 
 #include<iostream>
-#include"../shared/Value.h"
-#include"../shared/ArgFile.h"
-#include"../shared/CSVFile.h"
+#include"Value.h"
+#include"ArgFile.h"
+#include"CSVFile.h"
 
 using namespace std;
 
@@ -17,12 +17,12 @@ int main(){
 
 	args.load("arguments.txt");
 
-	if(args.isArguemnt("filename")){
+	if(args.isArgument("filename")){
 		Value val = args.getArgument("filename");
 		filename = val.asString();
 	}
 
-	vector<vector<Value > csv = file.readCSV(filename);
+	vector<vector<Value> > csv = file.readCSV(filename);
 	
 	return 0;
 }
