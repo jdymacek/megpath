71f3b5fc041b3a8c582bc7b9dd98a7991460e0e9
DaedricMD <dragonborn2649@gmail.com>
2017-05-26 11:13:34 -0400

    output patterns and coeffs to a file

--- testing/testnmf/driver.cpp
+++ testing/testnmf/driver.cpp
@@ -4,23 +4,46 @@
 //Last Modified: 5/26/2017
 
 #include <iostream>
+#include <random>
+#include <fstream>
 #include "../../../Eigen/Core"
 
 using namespace std;
 using namespace Eigen;
 
+void writeCSV(string filename, MatrixXd m){
+	ofstream outFile;
+	outFile.open(filename);
+
+	for(int i = 0; i < m.rows(); ++i){
+		for(int j = 0; j < m.cols(); ++j){
+			if(j < m.cols()-1){
+				outFile << m(i,j) << ",";
+			}else{
+				outFile << m(i,j);
+			}
+		}
+		outFile << "\n";
+	}
+	
+	outFile.close();
+}
+
 int main(){
 	MatrixXd patterns(3,5);
 	patterns << 0,0.15,0.2,0.15,0, 0,0.15,0.2,0.15,0, 0,0.15,0.2,0.15,0;
 
-	cout << patterns << endl;
-
-	for(int i = 0; i < patterns.rows(); ++i){
-		for(int j = 0; j < patterns.cols(); ++j){
-			cout << patterns(i,j) << " ";
+	default_random_engine gen;
+	uniform_real_distribution<double> dist(0.0,1.0);
+	MatrixXd coefficients(500,3);
+	for(int i = 0; i < coefficients.rows(); ++i){
+		for(int j = 0; j < coefficients.cols(); ++j){
+			coefficients(i,j) = dist(gen);
 		}
-		cout << "\n";
 	}
+
+	writeCSV("patterns.csv",patterns);
+	writeCSV("coefficients.csv",coefficients);
 
 	return 0;
 }
