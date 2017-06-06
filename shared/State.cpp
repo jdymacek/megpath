99c3f537c6186fbb89fae63e0c02e3fae0c08407
Julian Dymacek <jdymacek@gmail.com>
2017-06-06 10:48:15 -0400

    state fixes

--- shared/State.cpp
+++ shared/State.cpp
@@ -1,9 +1,163 @@
 #include "State.h"
 
-State::State(){}
+State::State(){
+	analysis = "";
+	directory = "";
+	filename = "";
+}
 
 
-void State::read(string filename){
+bool State::read(string argFileName){
+	int ROWS = 0;
+	int COLUMNS = 0;
+	int PATTERNS = 0;
+	
+	ArgFile args;
+	CSVFile file;
+	
+	vector<Value> origin;
+	vector<Value> controls;
+	vector<Value> columns;
+	vector<Value> patternArgs;
+		
+	//grab arguments
+	args.load(argFileName);
 
+	if(args.isArgument("analysis")){
+		Value val = args.getArgument("analysis");
+		analysis = val.asString();
+		analysis = analysis + "_";
+	}
 
+	if(args.isArgument("max_runs")){
+		Value val = args.getArgument("max_runs");
+		MAX_RUNS = val.asInt();
+	}
+
+	//cout << "Using analysis: " << analysis.substr(0,analysis.size()-1) << "\n";
+
+	if(args.isArgument(analysis + "filename")){
+		Value val = args.getArgument(analysis + "filename");
+		filename = val.asString();
+	}
+
+	if(args.isArgument(analysis + "patterns")){
+		Value val = args.getArgument(analysis + "patterns");
+		patternArgs = val.asVector();
+		PATTERNS = patternArgs.size();
+	}
+
+	if(args.isArgument(analysis + "origin")){
+		Value val = args.getArgument(analysis + "origin");
+		origin = val.asVector();
+	}else{
+		Value val;
+		origin.push_back(val);
+		origin.push_back(val);
+	}
+
+	if(args.isArgument(analysis + "directory")){
+		Value val = args.getArgument(analysis + "directory");
+		directory = val.asString();
+	}
+
+	if(args.isArgument(analysis + "controls")){
+		Value val = args.getArgument(analysis + "controls");
+		controls = val.asVector();
+	}
+
+	vector<vector<Value> > csv = file.readCSV(filename);
+
+	//expression matrix
+	ROWS = csv.size() - origin[1].asInt();
+	ROWS = csv.size() - origin[1].asInt();
+	if(args.isArgument(analysis + "columns")){
+		Value val = args.getArgument(analysis + "columns");
+		columns = val.asVector();
+		COLUMNS = columns.size();
+	}else{
+		COLUMNS = csv[0].size() - origin[0].asInt();
+		for(int i = 0; i < COLUMNS;++i){
+			Value newVal(i);
+			columns.push_back(newVal);
+		}
+	}
+
+	expression = MatrixXd(ROWS,COLUMNS);
+	expression = MatrixXd::Zero(ROWS,COLUMNS);
+	
+	if(columns.size() != controls.size() && args.isArgument(analysis + "controls")){
+		cout << "Columns and controls must be the same size.";
+		return false;
+	}
+
+	for(int i = 0; i < ROWS; ++i){
+		for(int j = 0; j < COLUMNS; ++j){
+			if(columns.size() == controls.size()){
+				expression(i,j) = csv[i+origin[1].asInt()][columns[j].asInt()+origin[0].asInt()].asDouble() - csv[i+origin[1].asInt()][controls[j].asInt()+origin[0].asInt()].asDouble();
+			}else{
+				expression(i,j) = csv[i+origin[1].asInt()][columns[j].asInt()+origin[0].asInt()].asDouble();
+			}
+		}
+	}
+
+	//cout << expression << endl;
+
+	normalize();
+
+	/*	cout << "After Normalizing:\n";
+		cout << expression << "\n";
+	 */
+
+	
+	//test arg information
+	/*cout << "directory = " << directory << endl;
+	cout << "max_runs = " << MAX_RUNS  << endl;
+	cout << "controls = [";
+	for(int i = 0; i < controls.size(); ++i){
+		cout << controls[i].asDouble() << " ";
+	}
+	cout << "] \n";
+	cout << "columns = [";
+	for(int i = 0; i < columns.size(); ++i){
+		cout << columns[i].asDouble() << " ";
+	}
+	cout << "] \n";
+	*/
+
+	//should be PATTERNS and COLUMNS
+
+	//how to fix error
+	patterns = NMFMatrix(PATTERNS,COLUMNS,&findErrorColumn);
+
+	//should be ROWS and PATTERNS
+	coefficients = NMFMatrix(ROWS,PATTERNS,&findErrorRow);
+
+	for(int i = 0; i < patternArgs.size(); ++i){
+		vector<Value> intoMatrix;
+		string findPattern = patternArgs[i].asString();
+		if(args.isArgument(findPattern)){
+			Value newVal = args.getArgument(findPattern);
+			intoMatrix = newVal.asVector();
+
+			ShiftPF* shared = new ShiftPF();			
+			vector<Entry> constraints(intoMatrix.size(),{0,0,0});
+			for(int j = 0; j < intoMatrix.size(); ++j){
+				patterns.matrix(i,j) = intoMatrix[j].asDouble();
+				patterns.functions[i][j] = shared;	
+				constraints[j].x = j;
+				constraints[j].y = i;
+				constraints[j].val = patterns.matrix(i,j);	
+			}
+			shared->setEntries(constraints);
+		}
+	}
+	return true;
+}
+
+void State::normalize(){
+	double max = expression.maxCoeff();
+	double min = expression.minCoeff();
+	expression = expression.array() - min;
+	expression = expression/(max-min);
 }
