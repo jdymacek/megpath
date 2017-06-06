26311e25545449a6297a4eb1b75f057c2d991ee8
Julian Dymacek <jdymacek@gmail.com>
2017-06-06 11:40:04 -0400

    created analysis

---
+++ shared/Analysis.cpp
@@ -0,0 +1,26 @@
+//Analysis.cpp 
+//Julian Dymacek
+//Created: 6/6/2017
+//A base class for different ConNMF algorithms
+
+
+#include "Analysis.h"
+
+Analysis::Analysis(){
+	state = NULL;
+}
+
+void Analysis::start(string filename){
+	//initialize global variables
+	state = new State();
+	state->load(filename);
+}
+
+void Analysis::run(){
+
+}
+
+void Analysis::stop(){
+
+}
+
