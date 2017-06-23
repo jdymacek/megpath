2f709a3e224808948419c0eab59a345ef1014c14
DaedricMD <dragonborn2649@gmail.com>
2017-06-23 09:35:57 -0400

    added quick patternMatching test

--- testing/state/driver.cpp
+++ testing/state/driver.cpp
@@ -1,3 +1,8 @@
+//State tester
+//Julian Dymacek
+//Matthew Dyer
+//Last Modified: 6/23/2017
+
 #include "State.h"
 
 int main(int argc, char** argv){
@@ -6,10 +11,18 @@ int main(int argc, char** argv){
 		
 	state->load(filename);
 
+	MatrixXd toMatch(3,5);
+	toMatch <<  0.6,0.4,0.2,0.1,0,
+					0.1,0.35,0.5,0.65,0.8,
+					0,0.15,0.4,0.15,0;
+
+	state->patternMatch(toMatch);
+	
+/*	
 	state->coefficients.resize(5,state->coefficients.columns);
 	MatrixXd expression = state->coefficients.matrix * state->patterns.matrix;
-
 	cout << expression << endl;
+*/
 	
 	return 0;
 }
