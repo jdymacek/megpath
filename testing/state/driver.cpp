185100d70ac1a024f4aff1d99f751dacbe6c5aac
DaedricMD <dragonborn2649@gmail.com>
2017-06-23 09:41:52 -0400

    updated tester

--- testing/state/driver.cpp
+++ testing/state/driver.cpp
@@ -11,18 +11,23 @@ int main(int argc, char** argv){
 		
 	state->load(filename);
 
+	cout << "original patterns: \n" << state->patterns.matrix;
+
 	MatrixXd toMatch(3,5);
 	toMatch <<  0.6,0.4,0.2,0.1,0,
 					0.1,0.35,0.5,0.65,0.8,
 					0,0.15,0.4,0.15,0;
 
+	cout << "patterns to match: \n" << toMatch;
+
 	state->patternMatch(toMatch);
+
+	cout << "after matching patterns: \n" << state->patterns.matrix;
 	
 /*	
 	state->coefficients.resize(5,state->coefficients.columns);
 	MatrixXd expression = state->coefficients.matrix * state->patterns.matrix;
 	cout << expression << endl;
-*/
-	
+*/	
 	return 0;
 }
