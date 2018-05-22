667c7abd1a5545b9b207ee41d5d7434c5b1bce5c
Julian Dymacek <jdymacek@gmail.com>
2018-05-22 13:08:50 -0400

    created barrier.h

---
+++ threaded/Barrier.h
@@ -0,0 +1,33 @@
+//https://stackoverflow.com/questions/24465533/implementing-boostbarrier-in-c11
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+class Barrier {
+public:
+    explicit Barrier(std::size_t iCount) : 
+      mThreshold(iCount), 
+      mCount(iCount), 
+      mGeneration(0) {
+    }
+
+    void Wait() {
+        unique_lock<std::mutex> lLock{mMutex};
+        auto lGen = mGeneration;
+        if (!--mCount) {
+            mGeneration++;
+            mCount = mThreshold;
+            mCond.notify_all();
+        } else {
+            mCond.wait(lLock, [this, lGen] { return lGen != mGeneration; });
+        }
+    }
+
+private:
+	mutex mMutex;
+    condition_variable mCond;
+    size_t mThreshold;
+    size_t mCount;
+    size_t mGeneration;
+};
