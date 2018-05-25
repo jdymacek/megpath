cffa829b0e89370ae348db0de3add55fe2ff3cba
Julian Dymacek <jdymacek@gmail.com>
2018-05-25 09:41:28 -0400

    standard fixed

--- standard/Standard.h
+++ standard/Standard.h
@@ -5,11 +5,9 @@
 #include "Analysis.h"
 
 using namespace std;
-class Standard: public Analysis, public Observer{
+class Standard: public Analysis{
 	public:
 		Standard();
-		virtual void monteCallback(double error);
-		virtual void annealCallback(double error);
 		virtual void run();
 		virtual void stop();
 };
