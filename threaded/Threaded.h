bb3c8c3e0c8532126ee483544aecf3d3267a6262
Julian Dymacek <jdymacek@gmail.com>
2018-05-25 09:43:46 -0400

    threaded fixed

--- threaded/Threaded.h
+++ threaded/Threaded.h
@@ -8,11 +8,12 @@ using namespace std;
 
 //thinking that the global state should be declaired outside of class say here:
 
-class Threaded:public Analysis, public Observer{
+class Threaded:public Analysis{
 	public:
 		Threaded(int nt);
+		virtual bool annealCallback(int iterations);
 		virtual void annealPrintCallback(int iterations);
-		virtual void montePrintCallback(int iterations):
+		virtual void montePrintCallback(int iterations);
 		virtual void start(string filename);
 		virtual void run();
 		virtual void stop();
