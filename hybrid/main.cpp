aa125ce71a8b2240e705919e4fdda948398ab3e1
Dakota Martin <dakota.martin@live.longwood.edu>
2018-06-15 14:21:26 -0400

    made main use the new cmdArgs file

--- hybrid/main.cpp
+++ hybrid/main.cpp
@@ -12,45 +12,55 @@
 #include "ParallelPatterns.h"
 #include "ParallelFuncThrow.h"
 #include "PatternMatching.h"
+#include "CmdArgs.h"
 
 using namespace std;
 
 int main(int argc, char** argv){
-	if(argc < 5){
-		cerr << "Needs an argument file and analysis to run!";
-		return 0;
-	}
-	string argFile = argv[1];
-	string analysis = argv[2];
-	string al = argv[3]; 
-	
+	CmdArgs args(argc,argv);
+	string argFile = "";
+	args.getAsString("a",argFile,"../testing/testnmf/test_arguments.txt");
+	string analysis = args.findFlag({{"0","dn", "DN", "DistNaive"},
+					  {"1","ft", "FT", "Functhrow"},
+					  {"2","pp", "PP", "ParPat", "ParallelPatterns"},
+					  {"3","pft","PFT","ParFuncThrow","ParallelFuncThrow"},
+					  {"4","pm","PM","PatMatch","PatternMatching"},
+					  {"5","test","Test"}});
+	string al = args.findFlag({{"0","t","T","Threaded"},
+				    {"1","tf","TF","FlipThreaded"},
+				    {"2","tp","TP","PhaseThreaded"}});
 	Analysis* a;
-
-	if(analysis == "DistNaive" || analysis == "DN" || analysis == "dn"){
-		a = new DistNaive();
-	}else if(analysis == "FuncThrow" || analysis == "FT" || analysis == "ft"){
-		a = new FuncThrow();
-	}else if(analysis == "ParallelPatterns" || analysis == "ParPat" || analysis == "PP" || analysis == "pp"){
-		a = new ParallelPatterns();
-	}else if(analysis == "ParallelFuncThrow" || analysis == "ParFuncThrow" || analysis == "PFT" || analysis == "pft"){
-		a = new ParallelFuncThrow();
-	}else if(analysis == "PatternMatching" || analysis == "PatMatch" || analysis == "PM" || analysis == "pm"){
-		a = new PatternMatching();
-	}else if(analysis == "Test" || analysis == "test"){
-		a = new Distributed();
+	switch(stoi(analysis)){
+		case 0:
+			a = new DistNaive();
+			break;
+		case 1:
+			a = new FuncThrow();
+			break;
+		case 2:
+			a = new ParallelPatterns();
+			break;
+		case 3:
+			a = new ParallelFuncThrow();
+			break;
+		case 4:
+			a = new PatternMatching();
+			break;
+		case 5:
+			a = new Distributed();
 	}
-
 	a->load(argFile);
-	int nt = thread::hardware_concurrency();
-	if(argc == 5){
-		nt = atoi(argv[4]);
-	}
-	if(al == "Threaded" || al == "T" || al == "t"){
-		a->setAlgorithm(new ThreadedMonteAnneal(a->state, nt));
-	}else if(al == "PhaseThreaded" || al == "PT" || al == "pt"){
-		a->setAlgorithm(new PhaseThreadedMonteAnneal(a->state, nt));
-	}else if(al == "FlipThreaded" || al == "FT" || al == "ft"){
-		a->setAlgorithm(new FlipThreadedMonteAnneal(a->state,nt));
+	int nt = args.getAsInt(al,to_string(thread::hardware_concurrency()));
+	switch(stoi(al)){
+		case 0:
+			a->setAlgorithm(new ThreadedMonteAnneal(a->state,nt));
+			break;
+		case 1:
+			a->setAlgorithm(new FlipThreadedMonteAnneal(a->state,nt));
+			break;
+		case 2:
+			a->setAlgorithm(new PhaseThreadedMonteAnneal(a->state,nt));
+			break;
 	}
 
 
