1871469c1797d718dac91061e2d16b04a7293257
Dakota Martin <dakota.martin@live.longwood.edu>
2018-06-18 08:56:29 -0400

    changed the main in distributed to use the new CmdArgs file

--- distributed/main.cpp
+++ distributed/main.cpp
@@ -9,32 +9,45 @@
 #include "ParallelPatterns.h"
 #include "ParallelFuncThrow.h"
 #include "PatternMatching.h"
+#include "CmdArgs.h"
 
 using namespace std;
 
 int main(int argc, char** argv){
-	if(argc < 3){
-		cerr << "Needs an argument file and analysis to run!";
-		return 0;
-	}
-
-	string argFile = argv[1];
-	string analysis = argv[2];
+	CmdArgs args(argc,argv);
+	string argFile = "";
+	args.getAsString("a",argFile,"../testing/testnmf/test_arguments.txt");
+	string analysis = args.findFlag({{"0","dn", "DN", "DistNaive"},
+					  {"1","ft", "FT", "Functhrow"},
+					  {"2","pp", "PP", "ParPat", "ParallelPatterns"},
+					  {"3","pft","PFT","ParFuncThrow","ParallelFuncThrow"},
+					  {"4","pm","PM","PatMatch","PatternMatching"},
+					  {"5","test","Test"}});
 	
+	if(args.wasFatal()){
+		cout << "Missing Args" << endl;
+		cout << args.errors() << endl;
+		exit(1);
+	}
 	Analysis* a;
-
-	if(analysis == "DistNaive" || analysis == "DN" || analysis == "dn"){
-		a = new DistNaive();
-	}else if(analysis == "FuncThrow" || analysis == "FT" || analysis == "ft"){
-		a = new FuncThrow();
-	}else if(analysis == "ParallelPatterns" || analysis == "ParPat" || analysis == "PP" || analysis == "pp"){
-		a = new ParallelPatterns();
-	}else if(analysis == "ParallelFuncThrow" || analysis == "ParFuncThrow" || analysis == "PFT" || analysis == "pft"){
-		a = new ParallelFuncThrow();
-	}else if(analysis == "PatternMatching" || analysis == "PatMatch" || analysis == "PM" || analysis == "pm"){
-		a = new PatternMatching();
-	}else if(analysis == "Test" || analysis == "test"){
-		a = new Distributed();
+	switch(stoi(analysis)){
+		case 0:
+			a = new DistNaive();
+			break;
+		case 1:
+			a = new FuncThrow();
+			break;
+		case 2:
+			a = new ParallelPatterns();
+			break;
+		case 3:
+			a = new ParallelFuncThrow();
+			break;
+		case 4:
+			a = new PatternMatching();
+			break;
+		case 5:
+			a = new Distributed();
 	}
 
 	a->load(argFile);
