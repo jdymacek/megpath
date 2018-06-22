dddf70cb94037e97ca1291827701a975dcbdfacb
Dakota Martin <dakota.martin@live.longwood.edu>
2018-06-22 15:41:14 -0400

    changed multication order

--- threeMatrix/ThreeErrorFunction.cpp
+++ threeMatrix/ThreeErrorFunction.cpp
@@ -6,6 +6,6 @@ ThreeErrorFunction::ThreeErrorFunction(ThreeState* st):ErrorFunction(st){
 
 double ThreeErrorFunction::error(){
 	newExpression.noalias() = state->expression;
-    newExpression.noalias() -= ((state->coefficients.matrix*state->patterns.matrix)*state->weights.matrix);
+    newExpression.noalias() -= ((state->coefficients.matrix*(state->weights.matrix*state->patterns.matrix)));
     return (newExpression.cwiseAbs()).sum();		
 }
