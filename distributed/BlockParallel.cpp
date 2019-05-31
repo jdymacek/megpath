e5e8d7e4eee11e7e41cda9145ee3aeeaa6ea49ef
Justin Moore <justin.moore1@live.longwood.edu>
2019-05-31 14:37:00 -0400

    Added column groups to Block Parallel

--- distributed/BlockParallel.cpp
+++ distributed/BlockParallel.cpp
@@ -25,7 +25,7 @@ void BlockParallel::start(){
 
 	if(rank == 0){
 		set<vector<int>> rowGroups;
-		bool same = false;
+		set<vector<int>>::iterator it;
 		for(int i = 0; i < state->expression.rows(); i++){
 			vector<int> row;
 			for(int j = 0; j < ar.size(); j++){
@@ -33,14 +33,47 @@ void BlockParallel::start(){
 					row.push_back(j);
 				}
 			}
-			for(int j = 0; j < row.size(); j++){
-				cout << row[j] << '\t';
-			}
-			cout << endl;
-			rowGroups.insert(row);
+//			for(int j = 0; j < row.size(); j++){
+//				cout << row[j] << '\t';
+//			}
+//			cout << endl;
+			if(row.size() > 0)
+				rowGroups.insert(row);
 		}
 
-		cout << "Set Amount: " << rowGroups.size() << endl;
+		cout << "Row Amount: " << rowGroups.size() << endl;
+		for(it = rowGroups.begin(); it != rowGroups.end(); it++){
+			const vector<int> rowOut = (*it);
+			for(int j = 0; j<rowOut.size(); j++){
+				cout << rowOut[j] << '\t';
+			}
+			cout << endl;
+		} 
+
+		set<vector<int>> colGroups;
+		for(int i = 0; i < state->expression.cols(); i++){
+			vector<int> col;
+			for(int j = 0; j < ar.size(); j++){
+				if(i >= ar[j].colStart && i <= ar[j].colEnd){
+					col.push_back(j);
+				}
+			}
+//			for(int j = 0; j < col.size(); j++){
+//				cout << col[j] << '\t';
+//			}
+//			cout << endl;
+			if(col.size() > 0)
+				colGroups.insert(col);
+		}
+
+		cout << "Col Amount: " << colGroups.size() << endl;
+		for(it = colGroups.begin(); it != colGroups.end(); it++){
+			const vector<int> colOut = (*it);
+			for(int j = 0; j<colOut.size(); j++){
+				cout << colOut[j] << '\t';
+			}
+			cout << endl;
+		} 
 	}
 	/*	//if(rank == 0){
 		oexpression = state->expression;
