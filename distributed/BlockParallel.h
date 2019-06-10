41e9f3dfc3d6b01d5da6be8e07ed779ad85e59e9
Julian Dymacek <jdymacek@gmail.com>
2019-06-10 15:45:26 -0400

    world group ranks

--- distributed/BlockParallel.h
+++ distributed/BlockParallel.h
@@ -6,6 +6,8 @@
 #ifndef BLOCKPARALLEL__H
 #define BLOCKPARALLEL__H
 
+#include <algorithm>
+#include <vector>
 #include "ParallelPatterns.h"
 
 using namespace std;
@@ -14,6 +16,20 @@ struct BlockSet{
 	MPI_Group group;
 	MPI_Comm comm;
 	Range subRange;
+
+	vector<int> worldRanks(){
+		MPI_Group worldGroup;
+		MPI_Comm_group(MPI_COMM_WORLD,&worldGroup);
+		int size = 0;
+		MPI_Group_size(group,&size);
+		vector<int> gRanks(size);
+		vector<int> wRanks(size);
+		iota(gRanks.begin(),gRanks.end(),0);
+		MPI_Group_translate_ranks(group,size,&gRanks[0],worldGroup,&wRanks[0]);
+		return wRanks;		
+	}
+
+
 };
 
 class BlockParallel: public ParallelPatterns{
